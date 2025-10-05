adb3e88aec2a7d021118a24115656388dbe1c412
saffornKurma <shreesha625@gmail.com>
2025-10-05 18:26:24 +0530

    added eureka server

--- order/src/main/java/dev/cloud/order/productClient/ProductClient.java
+++ order/src/main/java/dev/cloud/order/productClient/ProductClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product-service",url="${feign.client.product-service.url}")
+@FeignClient(name="product-service")
 public interface ProductClient {
 
     @GetMapping("/products/{id}")
